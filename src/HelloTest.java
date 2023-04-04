1c907541ed975308bacef84f8ae1229a2e2d9428
ImSoojeong <tnwjd701@gmail.com>
2023-04-04 16:38:53 +0900

    second commit

--- src/HelloTest.java
+++ src/HelloTest.java
@@ -2,5 +2,7 @@
 public class HelloTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Wordl");
+		System.out.println("Hello Word2");
+		System.out.println("Hello Word3");
 	}
 }
